66fecf17548642ffe035ab3f115d51bf49b8fbd4
scmmishra <scm.mymail@gmail.com>
2019-04-20 12:40:53 +0530

    feat: Added a basic Navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,21 @@
 <template>
   <div id="app" class="container mx-auto">
+  	<Navbar v-if="noNavbar"/>
     <router-view/>
   </div>
 </template>
+<script>
+import Navbar from '@/components/Navbar/Navbar.vue'
+
+export default {
+	name: 'App',
+	components: {
+		Navbar
+	},
+	computed: {
+		noNavbar: function () {
+			return !['login', 'register'].includes(this.$route.name)
+		}
+	}
+}
+</script>
---
+++ client/src/components/Navbar/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+	<nav class="flex py-2">
+		<div class="w-1/5 h-12 flex items-center">
+			<FeatherIcon :name="logo" class="p-2"/>
+			<span class="text-black tracking-wide uppercase font-bold text-sm">Nutmeg</span>
+		</div>
+		<div class="w-3/5 h-12 flex items-center">
+			<input type="search" name="dash-search" class="form-input max-w-xs" placeholder="Search [⌘ + K]">
+		</div>
+		<div class="w-1/5 h-12 flex flex-row-reverse items-center">
+			<div>
+	      <img
+	        class="relative cursor-pointer hover:shadow-md w-10 h-10 rounded-full mr-4"
+	        :src="profile.url"
+	        :alt="profile.name"
+	      >
+    	</div>
+		</div>
+	</nav>
+</template>
+<script>
+export default {
+	name: 'Navbar',
+	data() {
+    return {
+      profile: {
+        url:
+          "https://pbs.twimg.com/profile_images/1047479361819922437/yHgaCFdq_400x400.jpg",
+        name: "Shivam Mishra"
+      },
+      show: false
+    };
+  },
+	computed: {
+		logo: function() {
+			const logos = ['clipboard', 'coffee', 'edit-2']
+			return logos.randomElement()
+		}
+	}
+}
+</script>
