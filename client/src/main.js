3c4721221770dc3b0d5513c9f60cc889c625fb35
scmmishra <scm.mymail@gmail.com>
2019-04-20 12:40:38 +0530

    feat: Adds FeatherIcon component

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/tailwind.css'
+import FeatherIcon from '@/components/Feather/FeatherIcon'
 
 Vue.config.productionTip = false
+Vue.component('FeatherIcon', FeatherIcon)
+
+Array.prototype.randomElement = function () {
+    return this[Math.floor(Math.random() * this.length)]
+}
 
 new Vue({
   router,
