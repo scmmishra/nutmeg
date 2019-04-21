f524bb9da7d0d0343ecb376dfc0ac8291c2e8730
scmmishra <scm.mymail@gmail.com>
2019-04-21 22:01:24 +0530

    feat: Added NoteCard component

---
+++ client/src/components/Note/NoteCard.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="w-full bg-white rounded shadow overflow-hidden py-4 px-6 mb-5 hover:shadow-md">
+		<div>
+			<div class="font-bold text-xl mb-2">{{ note.title }}</div>
+			<p class="text-grey-darker text-base leading-normal">
+				{{ note.description }}
+			</p>
+		</div>
+		<div class="pt-3">
+			<span v-for="tag in note.tags" class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker hover:bg-grey-darkest hover:text-grey-lighter cursor-pointer mr-2">#{{ tag.name }}</span>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	name: "NoteCard",
+	props: ['note']
+}
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,79 @@
 <template>
-  <div class="flex h-screen">
-	<div class="w-full max-w-sm m-auto self-center pb-20">
-		<div class="text-center p-5">
-			<img alt="Vue logo" src="@/assets/nutmeg.svg" class="h-16 select-none">
-		</div>
+	<div class="h-screen">
+		<NoteCard v-for="note in notes" :note="note"/>
 	</div>
-</div>
 </template>
 
 <script>
-
+import NoteCard from '@/components/Note/NoteCard'
 export default {
-  name: 'home',
+	name: 'home',
+	data () {
+		return {
+			notes: [
+				{
+					id: '143423423',
+					title: 'A Modest Proposal',
+					description: 'A Modest Proposal for preventing the children of poor people in Ireland, from being a burden on their parents or country, and for making them beneficial to the publick.',
+					tags: [
+						{
+							name: 'essay'
+						},
+						{
+							name: 'literature'
+						},
+						{
+							name: 'inspiration'
+						}
+					]
+				},
+				{
+					id: '143234242',
+					title: 'The Value of Science',
+					description: 'From time to time people suggest to me that scientists ought to give more consideration to social problems - especially that they should be more responsible in considering the impact of science on society.',
+					tags: [
+						{
+							name: 'inspiration'
+						},
+						{
+							name: 'science'
+						}
+					]
+				},
+				{
+					id: '647687659786',
+					title: 'The Commencement Speech',
+					description: 'I am honored to be with you today at your commencement from one of the finest universities in the world. I never graduated from college. Truth be told, this is the closest I’ve ever gotten to a college graduation.',
+					tags: [
+						{
+							name: 'inspiration'
+						},
+						{
+							name: 'startup'
+						},
+						{
+							name: 'apple'
+						}
+					]
+				},
+				{
+					id: '45845634263',
+					title: 'I Have a Dream',
+					description: 'I am happy to join with you today in what will go down in history as the greatest demonstration for freedom in the history of our nation.',
+					tags: [
+						{
+							name: 'freedom'
+						},
+						{
+							name: 'history'
+						}
+					]
+				}
+			]
+		}
+	},
+	components: {
+		NoteCard
+	}
 }
 </script>
